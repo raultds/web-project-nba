1c72a942d5e9066ab68bbfa8d4323081737fa8a1
raultds <rrj1@alumnes.udl.cat>
2019-04-11 19:29:08 +0200

    First big commit

---
+++ NBAstats/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from NBAstats import models
+from django.contrib.auth.models import User
+# Register your models here.
+admin.site.register(models.team)
+admin.site.register(models.player)
+admin.site.register(models.conference)
---
+++ script_player_csv.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+"""
+    Script to import book data from .csv file to Model Database DJango
+    To execute this script run:
+                                1) manage.py shell
+                                2) exec(open('script_player_csv.py').read())
+"""
+
+import csv
+from NBAstats.models import player
+
+CSV_PATH = 'players.csv'
+
+contSuccess = 0
+# Remove all data from Table
+player.objects.all().delete()
+
+with open(CSV_PATH, newline='') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=';', quotechar=';')
+    print('Loading...')
+    for row in spamreader:
+        print(row)
+        player.objects.create(player_id=row[0], name=row[1], last_name=row[2], jersey_num=row[3], position=row[4], height = row[5], weight=row[6], birth_date=row[7], birth_city=row[8], birth_country=row[9], team_id=row[10], team_abbr=row[11], team_city=row[12], team_name=row[13], player_photo=row[14])
+        contSuccess += 1
+    print('{str(contSuccess)} inserted successfully! ')
