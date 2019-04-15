ac1a2e99938a1330ca57f8d36a58b1b832e8ceee
Raul Roca Juncà <rrj1@alumnes.udl.cat>
2019-04-15 19:37:53 +0200

    Another deploy try

--- myStats/settings.py
+++ myStats/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STAT = os.path.join(BASE_DIR, 'static')
@@ -136,5 +136,5 @@ STAT,
 ]
 
 # Configure Django App for Heroku.
-import django_heroku
+
 django_heroku.settings(locals())
