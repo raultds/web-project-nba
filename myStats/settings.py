cab5036effcbedb3a006fe9a212e8a6ea42aad98
Raul Roca Juncà <rrj1@alumnes.udl.cat>
2019-04-15 20:19:51 +0200

    Hopefuly LAST DEPLOY TRY

--- myStats/settings.py
+++ myStats/settings.py
@@ -14,10 +14,7 @@ import os
 import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STAT = os.path.join(BASE_DIR, 'NBAstats/static')
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL ='/static/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
