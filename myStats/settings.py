57e75f154c8a3ea726515881ea34db1d728afda8
Raul Roca Juncà <rrj1@alumnes.udl.cat>
2019-04-15 20:07:26 +0200

    Another deploy try

--- myStats/settings.py
+++ myStats/settings.py
@@ -130,10 +130,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-STAT,
-]
+
 
 # Configure Django App for Heroku.
 
