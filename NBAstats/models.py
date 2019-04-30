c25e7b0dfb987ead3765b28001f83fd5f7f03dc9
ar98nau <anr3@alumnes.udl.cat>
2019-04-30 09:42:14 +0200

    model all_star

--- NBAstats/models.py
+++ NBAstats/models.py
@@ -53,30 +53,23 @@ class player(models.Model):
     def get_absolute_url(self):
         return reverse('myStats:player_detail', kwargs={'pk': self.player_id})
 
-class rookies(models.Model):
-    #fer que cada equip de rookies s'identifiqui per l'usuari(id) que el crea
-    #Enlloc de ficar el nom sol, fem que es guardi un accés al jugador
-        #així inclus podem tenir la foto.
-    #el problema sera lligar cada jugador amb el boto que els ficara i treurà de la taula
-        #segur que cal fer javaScript amb un listener del boto i merdes
-    #Mirar això del CASCADE perque ens interessa treure jugadors de la taula per fer que sigui dinàmic
-        #però no ens interessa borar tot el jugador de les altres basses de dades
-    #???? user_id = models.ForeignKey(User, blank=False, primary_key=True)
-    player1 = models.ForeignKey(player, null=True ,on_delete=models.CASCADE)
-    player2 = models.ForeignKey(player, null=True ,on_delete=models.CASCADE)
-    player3 = models.ForeignKey(player, null=True ,on_delete=models.CASCADE)
-    player4 = models.ForeignKey(player, null=True ,on_delete=models.CASCADE)
-    player5 = models.ForeignKey(player, null=True ,on_delete=models.CASCADE)
-    player6 = models.ForeignKey(player, null=True ,on_delete=models.CASCADE)
-    player7 = models.ForeignKey(player, null=True ,on_delete=models.CASCADE)
-    player8 = models.ForeignKey(player, null=True ,on_delete=models.CASCADE)
-    player9 = models.ForeignKey(player, null=True ,on_delete=models.CASCADE)
-    player10 = models.ForeignKey(player, null=True ,on_delete=models.CASCADE)
-    player11 = models.ForeignKey(player, null=True ,on_delete=models.CASCADE)
-    player12 = models.ForeignKey(player, null=True ,on_delete=models.CASCADE)
+class all_star(models.Model):
+    user_id = models.ForeignKey(User, blank=False, unique=True, on_delete=models.CASCADE)
+    player_1 = models.ForeignKey(player, related_name='player_1', null=True ,on_delete=models.SET_NULL)
+    player_2 = models.ForeignKey(player, related_name='player_2', null=True ,on_delete=models.SET_NULL)
+    player_3 = models.ForeignKey(player, related_name='player_3', null=True ,on_delete=models.SET_NULL)
+    player_4 = models.ForeignKey(player, related_name='player_4', null=True ,on_delete=models.SET_NULL)
+    player_5 = models.ForeignKey(player, related_name='player_5', null=True ,on_delete=models.SET_NULL)
+    player_6 = models.ForeignKey(player, related_name='player_6', null=True ,on_delete=models.SET_NULL)
+    player_7 = models.ForeignKey(player, related_name='player_7', null=True ,on_delete=models.SET_NULL)
+    player_8 = models.ForeignKey(player, related_name='player_8', null=True ,on_delete=models.SET_NULL)
+    player_9 = models.ForeignKey(player, related_name='player_9', null=True ,on_delete=models.SET_NULL)
+    player_10 = models.ForeignKey(player, related_name='player_10', null=True ,on_delete=models.SET_NULL)
+    player_11 = models.ForeignKey(player, related_name='player_11', null=True ,on_delete=models.SET_NULL)
+    player_12 = models.ForeignKey(player, related_name='player_12', null=True ,on_delete=models.SET_NULL)
 
     def __str__(self):
-        return self.user_id + ' ' + self.user_id.name #??????????????????
+        return self.user_id.pk + ' ' + self.user_id.name
 
     def get_absolute_url(self):
-        return reverse('myStats:rookies', kwargs={'pk': self.user_id}) #??????????
+        return reverse('myStats:all_star', kwargs={'pk': self.user_id})
